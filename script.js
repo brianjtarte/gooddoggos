029a740fdb4344a8048cf1e210ab0adc421ce9c5
Brian Tarte <brianjtarte@gmail.com>
2021-06-29 18:01:31 -0700

    made changes to script, added second js file

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 'use strict'
 
-let answer = prompt('Do you like dogs?');
-let greeting = '';
 
-if(answer === 'yes'){
-    greeting = 'Wonderful! You are in the right place!';
+
+function getanswer(){
+
+    let answer = prompt('Do you like dogs?');
+    let greeting = '';
+    
+    if(answer === 'yes'){
+        greeting = 'Wonderful! You are in the right place!';
+    }
+    else{
+        greeting = 'Even if you don\'t like dogs, you are welcome to stay!'; 
+        alert("WARNING: THIS SITE IS ABOUT DOGS!");
+    }
+    return greeting;
 }
-else{
-    greeting = 'Even if you don\'t like dogs, you are welcome to stay!'; 
-    alert("WARNING: THIS SITE IS ABOUT DOGS!");
-}
 
-document.write(greeting);
 
+
+
+
+
+
+document.write(getanswer());
---
+++ script2.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function fnamelname(){
+    let name1 = prompt('what is your first name?');
+    let name2 = prompt('what is your last name?');
+    
+  
+    function hello() {
+      return ' Hello ' + name1 + ' ' + name2 + ' ' + ' how are you today?';
+  
+    }
+  
+  return hello();
+  
+  }
+  
+  
+  document.write(fnamelname());
